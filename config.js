f4ef9546bec4cd93cfc022f210c66fee891a5507
Kent&Kclaw <skybomb77@gmail.com>
2026-04-19 07:54:40 +0800

    Add config.js + connect API

---
+++ config.js
@@ -0,0 +1,11 @@
+// ToneForge 配置
+const CONFIG = {
+    api: "http://localhost:5001",
+    name: "ToneForge",
+    version: "1.0.0"
+};
+
+// 自動偵測：本地開發 vs 生產
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+    CONFIG.api = 'http://localhost:5001';
+}
